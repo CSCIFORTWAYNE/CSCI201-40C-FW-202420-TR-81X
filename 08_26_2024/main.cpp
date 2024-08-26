a001926399b34fec8b7a5800c40c2223a54a8462
Angela Venable <avenable11@ivytech.edu>
2024-08-26 14:28:15 -0400

    with lecture activity

--- 08_26_2024/main.cpp
+++ 08_26_2024/main.cpp
@@ -1,14 +1,17 @@
 #include "person.h"
 #include "employee.h"
+#include <iostream>
 
+// add data entry to main to get employee information
+// add a comment with your test input
+// create the employee
+// call the to string
 int main()
 {
     person p("john", "doe");
     employee e("jane", "doe", "123-45-6789", "2024-02-26");
-    p.fname;
-    e.fname;
-    p.getFname();
-    e.getFname();
 
+    std::cout << p.tostring() << std::endl;
+    std::cout << e.tostring() << std::endl;
     return 0;
 }
--- 08_26_2024/person.cpp
+++ 08_26_2024/person.cpp
@@ -1,8 +1,30 @@
 #include "person.h"
 
+person::person(std::string f, std::string l, std::string a, std::string p, std::string e)
+{
+    fname = f;
+    lname = l;
+    address = a;
+    phone = p;
+    email = e;
+}
+
 std::string person::tostring() const
 {
     std::string output;
-    output = "First name: " + fname;
-    return std::string();
+    output = "First name: " + fname + "\n";
+    output += "Last name: " + lname + "\n";
+    if (!address.empty())
+    {
+        output += "Address: " + address + "\n";
+    }
+    if (!phone.empty())
+    {
+        output += "Phone: " + phone + "\n";
+    }
+    if (!email.empty())
+    {
+        output += "Email: " + email + "\n";
+    }
+    return output;
 }
